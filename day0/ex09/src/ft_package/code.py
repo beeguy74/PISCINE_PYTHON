71ff1fba4e73736f1a5e0094ac35a7f400ed06b3
Iaroslav NEVEROV <59573868+beeguy74@users.noreply.github.com>
2024-02-26 12:32:54 +0100

    Update code.py
    docstring was added

--- day0/ex09/src/ft_package/code.py
+++ day0/ex09/src/ft_package/code.py
@@ -1,4 +1,8 @@
 def count_in_list(items, arg) -> int:
+    '''
+    Count occurrences of argumnet "arg" in collection "items",
+    returning number of counted matches
+    '''
     res = 0
     for x in items:
         res += 1 if x == arg else 0
