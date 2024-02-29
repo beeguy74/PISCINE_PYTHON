444b6fcf4b5cf24d0454db301974e0119bb92ed4
Iaroslav NEVEROV <beeguy74@gmail.com>
2024-02-29 10:41:50 +0100

    minor improvement

--- day01/ex03/zoom.py
+++ day01/ex03/zoom.py
@@ -3,20 +3,30 @@ from load_image import ft_load
 import matplotlib.pyplot as plt
 
 
-def ft_zoom(image: np.ndarray) -> np.ndarray:
-    ''' print some information
-      about image and display it after "zooming"'''
-    return image[-601:-201, -501:-101, 2]
+def ft_zoom(
+        image: np.ndarray,
+        lines: tuple[int, int], rows: tuple[int, int],
+        pixels: tuple[int, int]
+        ) -> np.ndarray:
+    '''slicing image array AKA "zooming"'''
+    return image[lines[0]: lines[1], rows[0]: rows[1], pixels[0]: pixels[1]]
 
 
 def main():
-    # your tests and your error handling
-    file = ft_load("animal.jpg")
-    res = ft_zoom(file)
-    print(np.shape(res))
-    print(res)
-    plt.imshow(res, cmap="viridis", interpolation="nearest")
-    plt.show()
+    '''
+    load the image "animal.jpeg", print some information
+    about it and display it after "zooming".
+    '''
+    try:
+        file = ft_load("animal.jpg")
+        print(file)
+        res = ft_zoom(file, (-601, -201), (-501, -101), (1, 2))
+        print(f'New shape after slicing: {res.shape} or {res.shape[0: 2]}')
+        print(res)
+        plt.imshow(res, cmap='gray')
+        plt.show()
+    except Exception as e:
+        print("Error ft_load:", e)
 
 
 if __name__ == "__main__":
