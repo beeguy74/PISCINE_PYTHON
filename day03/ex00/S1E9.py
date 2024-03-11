e4cb501b19285b5d841fb94bb171403c65ff617e
Iaroslav NEVEROV <59573868+beeguy74@users.noreply.github.com>
2024-03-11 18:18:44 +0100

    ex00 abstract classe

--- day03/ex00/S1E9.py
+++ day03/ex00/S1E9.py
@@ -1,15 +1,27 @@
 from abc import ABC, abstractmethod
-    
+
 
     class Character(ABC):
-        """Your docstring for Class"""
+        """Abstract class with with
+        constructor and abstract method die"""
+        def __init__(self, first_name, is_alive=True) -> None:
+            """takes first_name as first parameter,
+            is_alive as second non mandatory parameter
+            set to True by default"""
+            self.first_name = first_name
+            self.is_alive = is_alive
+
         @abstractmethod
         def die(self):
+            """this method should change health state"""
             pass
-            """Your docstring for Class"""
-            #your code here
 
 
     class Stark(Character):
-    """Your docstring for Class"""
-        #your code here
+        """child of Character class, implements method die"""
+        def __init__(self, first_name, is_alive=True) -> None:
+            super().__init__(first_name, is_alive)
+
+        def die(self) -> None:
+            """set an attribute is_alive to False"""
+            self.is_alive = False
