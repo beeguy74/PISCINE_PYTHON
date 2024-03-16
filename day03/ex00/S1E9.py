d8caa335323dde9ec5c9c6b7f3efa67ea7891f0a
Iaroslav NEVEROV <beeguy74@gmail.com>
2024-03-16 17:34:07 +0100

    ex00

--- day03/ex00/S1E9.py
+++ day03/ex00/S1E9.py
@@ -1,27 +1,30 @@
 from abc import ABC, abstractmethod
 
 
-    class Character(ABC):
-        """Abstract class with with
-        constructor and abstract method die"""
-        def __init__(self, first_name, is_alive=True) -> None:
-            """takes first_name as first parameter,
-            is_alive as second non mandatory parameter
-            set to True by default"""
-            self.first_name = first_name
-            self.is_alive = is_alive
+class Character(ABC):
+    """class Character: Abstract class with with
+    constructor and abstract method die"""
+    def __init__(self, first_name, is_alive=True) -> None:
+        """Character's constructor: takes first_name as first parameter,
+        is_alive as second non mandatory parameter
+        set to True by default"""
+        self.first_name = first_name
+        self.is_alive = is_alive
 
-        @abstractmethod
-        def die(self):
-            """this method should change health state"""
-            pass
+    @abstractmethod
+    def die(self):
+        """Character's abstract method die(): this method should change health
+        state"""
+        pass
 
 
-    class Stark(Character):
-        """child of Character class, implements method die"""
-        def __init__(self, first_name, is_alive=True) -> None:
-            super().__init__(first_name, is_alive)
+class Stark(Character):
+    """class Stark: child of Character class, implements method die"""
+    def __init__(self, first_name, is_alive=True) -> None:
+        """Stark's constructor: uses Character's constructor
+        to set first_name and is_alive"""
+        super().__init__(first_name, is_alive)
 
-        def die(self) -> None:
-            """set an attribute is_alive to False"""
-            self.is_alive = False
+    def die(self) -> None:
+        """Stark's method die() set an attribute is_alive to False"""
+        self.is_alive = False
--- day03/ex00/tester.py
+++ day03/ex00/tester.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-
 from S1E9 import Character, Stark
 
 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+
+
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+
+
+def main():
+    prYellow('\n00 test from subject')
     Ned = Stark("Ned")
     print(Ned.__dict__)
     print(Ned.is_alive)
@@ -15,3 +22,14 @@ from S1E9 import Character, Stark
     print("---")
     Lyanna = Stark("Lyanna", False)
     print(Lyanna.__dict__)
+
+    prYellow("\n01 trying to create an Character's instance")
+    try:
+        hodor = Character("hodor")
+        print(hodor.__dict__)
+    except TypeError as e:
+        print("TypeError: ", e)
+
+
+if __name__ == "__main__":
+    main()
